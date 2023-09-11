b5229ccc47ce786ab19d19c842bef39a76c7d3f1
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2023-09-11 18:28:51 +0800

    Add loggings

--- main.sh
+++ main.sh
@@ -4,4 +4,8 @@ host=$1
 
 [ -z ${host} ] && echo "Missing host" >&2 && exit 1
 
+echo "Listening..."
+echo "SOCKET_PATH: ${SOCKET_PATH}"
+echo "HOST: ${host}"
+
 socat UNIX-LISTEN:${SOCKET_PATH},fork,reuseaddr,unlink-early TCP:${host}
